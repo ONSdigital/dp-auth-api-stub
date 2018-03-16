f9446eb07a8da797616f7a5ebc512c5ede143543
Dave Llewellyn <439837@gmail.com>
2018-03-16 17:12:59 +0000

    initial impl of stubbed service

---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+	"github.com/ONSdigital/go-ns/log"
+	"github.com/ONSdigital/go-ns/server"
+	"os"
+	"os/signal"
+	"syscall"
+	"context"
+	"time"
+	"github.com/ONSdigital/dp-auth-api-stub/identity"
+)
+
+func main() {
+	log.Namespace = "dp-auth-api-stub"
+
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, os.Interrupt, syscall.SIGTERM)
+	errorChan := make(chan error, 1)
+
+	stub, err := identity.NewStub()
+	if err != nil {
+		log.ErrorC("failed to create stub, exiting", err, nil)
+		os.Exit(1)
+	}
+
+	router := mux.NewRouter()
+	router.Path("/identity").Methods(http.MethodGet).HandlerFunc(stub.Handle)
+
+	httpServer := server.New(":8082", router)
+
+	go func() {
+		log.Info("starting http server", nil)
+		if err := httpServer.ListenAndServe(); err != nil {
+			errorChan <- err
+		}
+	}()
+
+	select {
+	case err := <-errorChan:
+		log.ErrorC("application error encountered, commencing graceful shutdown", err, nil)
+		ctx, _ := context.WithTimeout(context.Background(), time.Second*5)
+		if err := httpServer.Shutdown(ctx); err != nil {
+			log.ErrorC("graceful shutdown failed", err, nil)
+		}
+	case sig := <-signals:
+		log.Info("received os signal, commencing graceful shutting down", log.Data{"signal": sig.String()})
+		ctx, _ := context.WithTimeout(context.Background(), time.Second*5)
+		if err := httpServer.Shutdown(ctx); err != nil {
+			log.ErrorC("graceful shutdown failed", err, nil)
+		}
+	}
+
+}
